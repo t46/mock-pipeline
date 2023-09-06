5994db3845648ad72e76901ad051d9b8ab2a0d8b
t46 <takagi4646@gmail.com>
2023-09-06 16:32:15 +0900

    delete commented out parts

--- pipeline.py
+++ pipeline.py
@@ -7,58 +7,33 @@ import warnings
 
 warnings.simplefilter('ignore')
 
-def main(args):
+def main():
     now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     logging.basicConfig(
         filename=f'logs/{now}.log',
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s'
 )
-    llm = OpenAI(model_name="gpt-4", temperature=0.0)  # TODO: define internally
-    verification_result = 'False'
+    llm = OpenAI(model_name="gpt-4", temperature=0.0)
 
-    # NOTE: for now, we assume that the problem is given as an input
-    # paper = args.path_to_paper
-    # problem_discoverer = problem_discovery.ProblemDiscoverer(llm)
-    # problem = problem_discoverer(paper)
-    # print(problem)
-    # logging.info('Problem: %s', problem)
-
-    # del problem_discoverer
     with open('input_data/problem_2.txt') as f:
         problem = f.read()
         print(problem)
         logging.info('Problem: %s', problem)
 
-    cnt = 0
 
-    while 'False' in verification_result:
+    hypothesis_generator = hypothesis_generation.HypothesisGenerator(llm)
+    hypothesis = hypothesis_generator(problem)
+    print(hypothesis)
+    logging.info('Hypothesis: %s', hypothesis)
 
-        hypothesis_generator = hypothesis_generation.HypothesisGenerator(llm)
-        if cnt == 0:
-            hypothesis = hypothesis_generator(problem)
-        else:
-            hypothesis = hypothesis_generator(problem, hypothesis)
-        print(hypothesis)
-        logging.info('Hypothesis: %s', hypothesis)
+    del hypothesis_generator
 
-        del hypothesis_generator
+    verification_executor = verification_execution.VerificationExecutor(llm)
+    verification_result = verification_executor(problem, hypothesis)
+    logging.info('Verification Result: %s', verification_result)
 
-        verification_executor = verification_execution.VerificationExecutor(llm)
-        verification_result = verification_executor(problem, hypothesis)
-        logging.info('Verification Result: %s', verification_result)
-
-        del verification_executor
-
-        if 'True' in verification_result:
-            print('Hypothesis is True')
-            break
-
-        cnt += 1
-
-        # debugging
-        if cnt == 1:
-            break
+    del verification_executor
 
     verification_plan = 'TODO: define internally'
 
@@ -68,7 +43,4 @@ def main(args):
     logging.info('LaTeX Content: %s', latex_content)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--path_to_paper", type=str, default='input_data/paper.tex', help="path_to_paper")
-    args = parser.parse_args()
-    main(args)
+    main()
